5583da79cbea781d50e631cc87a2343fd9a77aa4
liuDLs7 <130816440+liuDLs7@users.noreply.github.com>
2025-01-11 22:42:33 +0800

    add dict process method

--- tiaoshi.py
+++ tiaoshi.py
@@ -2,20 +2,6 @@
 import inspect
 import pdb
 import os
-import sys
-import tty
-import termios
-import select
-
-# def clear_input_buffer():
-#     fd = sys.stdin.fileno()
-#     old_settings = termios.tcgetattr(fd)
-#     try:
-#         tty.setraw(fd)  # 设置为原始模式
-#         while select.select([sys.stdin], [], [], 0)[0]:
-#             sys.stdin.read(1)  # 读取并丢弃输入
-#     finally:
-#         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)  # 恢复设置
 
 def get_vname():
     frame = inspect.currentframe()  # 当前函数的栈帧
@@ -31,7 +17,7 @@ def get_vname():
     else:
         print("No variable name found.", end='-'*10)
         print_caller_file_and_line(3)
-
+ 
 def print_caller_file_and_line(depth=2):
     # 获取调用栈信息
     stack = inspect.stack()
@@ -42,86 +28,95 @@ def print_caller_file_and_line(depth=2):
     print(f"调试文件: {caller_file}, 所在行号: {caller_line}")
 
 def tiaoshi(x=None, exit0=True, details=False, mulp=False, get_name=True, use_debug=True):
-	# 无法使用全局变量，仍需修改
-	# if first_call:
-	# 	clear_input_buffer()
-	# 	first_call = False
-	if not use_debug:
-		return
-	if get_name:
-		get_vname()
-	print(type(x))
-	if isinstance(x,str):
-		print(x)
-		if not mulp:
-			t = input('continue?(stop or else)')
-			if t == 'stop':
-				exit(0)
-		return
-	try:
-		x[0]
-	except:
-		print(x)
-		if not mulp:
-			t = input('continue?(stop or else)')
-			if t == 'stop':
-				exit(0)
-		if exit0:
-			print_caller_file_and_line(2)
-			exit(0)
-		else:
-			return
-	try:
-		print(x.shape)
-	except:
-		try:
-			print(len(x))
-			if not mulp:
-				if isinstance(x,list):
-					if not all(type(y) == type(x[0]) for y in x):
-						if input('Customized Warning: the list has different data types! show them all?(y/n)') in ['y','Y']:
-							for ele in x:
-								print(type(ele))
-					printlen = input("print how much elements' shape? (default 0) ")
-					if printlen.isdigit():
-						for idx in range(min(len(x),int(printlen))):
-							try:
-								print(x[idx].shape)
-							except:
-								print(f"can't get x[{idx}] shape,  x[{idx}] has type: {type(x[idx])}")
-								t = input('continue?(stop or else)')
-								if t == 'stop':
-									exit(0)
-		except:
-			print("can't get shape or len")
-	if not mulp:
-		t = input('show details?(y/n)')
-		if t == 'pdb':
-			try:
-				pdb.set_trace()  # 设置断点
-			except:
-				pass
-		elif t == 'stop':
-			exit(0)
-		elif t in ['y', 'Y']:
-			details = True
-		if details:
-			print(x)
-		# 更深入查看
-		deep_visit = input('show details for sub elements?(y/n)')
-		visit_remember = []
-		while deep_visit in ['y', 'Y']:
-			idx = input(f'to visit which element?(range[0-{len(x)-1}], default 0)')
-			if idx.isdigit() and int(idx) < len(x):
-				idx = int(idx)
-			else:
-				idx = 0
-			print(f"{'*'*10}now visit x[{idx}]{'*'*10}")
-			tiaoshi(x[idx], exit0=False, get_name=False)
-			visit_remember.append(idx)
-			print(f"{'*'*10}x{set(visit_remember)} has been visited{'*'*10}")
-			deep_visit = input('show details for sub elements?(y/n)')
-	if exit0:
-		print_caller_file_and_line(2)
-		exit(0)
+    if not use_debug:
+        return
+    if get_name:
+        get_vname()
+    print(type(x))
+    if isinstance(x,str):
+        print(x)
+        t = input('continue?(stop or else)')
+        if t == 'stop':
+            exit(0)
+        return
+    try:
+        x[0]
+    except:
+        if isinstance(x,dict):
+            ks = list(x.keys())
+            print(ks)
+            t = input('show dict details?(y/n) ')
+            while t in ['y','Y']:
+                tt = input('which key to visit?(input key name or index) ')
+                try:
+                    tiaoshi(x[tt],exit0=False)
+                except:
+                    if tt.isdigit() and int(tt) < len(ks):
+                        tt = int(tt)
+                        tt = ks[tt]
+                        print(f'{"*"*10}visit key "{tt}"{"*"*10}')
+                        tiaoshi(x[tt],exit0=False)
+                    else:
+                        print('no such key in dict!')
+                t = input('continue show dict details?(y/n) ')
+            return
+        else:
+            print(x)
+            t = input('continue?(stop or else)')
+            if t == 'stop':
+                exit(0)
+            if exit0:
+                print_caller_file_and_line(2)
+                exit(0)
+            else:
+                return
+    try:
+        print(x.shape)
+    except:
+        try:
+            print(len(x))
+            if isinstance(x,list):
+                if not all(type(y) == type(x[0]) for y in x):
+                    if input('Customized Warning: the list has different data types! show them all?(y/n)') in ['y','Y']:
+                        for ele in x:
+                            print(type(ele))
+                printlen = input("print how much elements' shape? (default 0) ")
+                if printlen.isdigit():
+                    for idx in range(min(len(x),int(printlen))):
+                        try:
+                            print(x[idx].shape)
+                        except:
+                            print(f"can't get x[{idx}] shape,  x[{idx}] has type: {type(x[idx])}")
+                            t = input('continue?(stop or else)')
+                            if t == 'stop':
+                                exit(0)
+        except:
+            print("can't get shape or len")
+    if not mulp:
+        t = input('show details?(y/n)')
+        if t == 'pdb':
+            pdb.set_trace()
+        elif t == 'stop':
+            exit(0)
+        elif t in ['y', 'Y']:
+            details = True
+        if details:
+            print(x)
+        # 更深入查看
+        deep_visit = input('show details for sub elements?(y/n)')
+        visit_remember = []
+        while deep_visit in ['y', 'Y']:
+            idx = input(f'to visit which element?(range[0-{len(x)-1}], default 0)')
+            if idx.isdigit() and int(idx) < len(x):
+                idx = int(idx)
+            else:
+                idx = 0
+            print(f"{'*'*10}now visit x[{idx}]{'*'*10}")
+            tiaoshi(x[idx], exit0=False, get_name=False)
+            visit_remember.append(idx)
+            print(f"{'*'*10}x{set(visit_remember)} has been visited{'*'*10}")
+            deep_visit = input('show details for sub elements?(y/n)')
+    if exit0:
+        print_caller_file_and_line(2)
+        exit(0)
 #############################################################
